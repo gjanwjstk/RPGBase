2078ca487555b2d862bde4c9509969c8860adf0e
MoonJihwan <moasa11@naver.com>
2016-11-07 23:26:55 +0900

    마지막 CS파일 저장

--- Assets/Scripts/FieldManager.cs
+++ Assets/Scripts/FieldManager.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+public class FieldManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject owner_field;
+    [SerializeField]
+    GameObject[] enemys;
+    [SerializeField, Range(1, 5)]
+    int field_level;
 
-public class FieldManager : MonoBehaviour {
+    List<Enemy> enemy_list;
+    int max_enemy;
+    void Awake()
+    {
+        enemy_list = new List<Enemy>();
+        max_enemy = 10;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        for (int i = 0; i < 5; i++)
+        {
+            Create_Enemy();
+        }
+        InvokeRepeating("Update_FieldEnemy", 1.0f, 5.0f);
+    }
+    void Update_FieldEnemy()
+    {
+        if (enemy_list.Count < max_enemy)
+            Create_Enemy();
+    }
+    void Create_Enemy()
+    {
+        int enemy_type = Random.Range(0, enemy)
+    }
+    public void Delete_Enemy(Enemy _enemy)
+    {
+
+    }
 }
